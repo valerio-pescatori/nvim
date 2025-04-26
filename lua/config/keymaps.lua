108e03d507102a371d802b0cd53e54321c70db59
valerio-pescatori <pescatorim14@gmail.com>
2025-04-26 17:20:32 +0200

    initial commit

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,16 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+vim.keymap.set("n", "s", "<Nop>")
+vim.keymap.set("x", "s", "<Nop>")
+-- Go to start of line in normal and visual modes
+vim.keymap.set({ "n", "v" }, "gs", "^", { desc = "Go to Start of Line" })
+
+-- Go to end of line in normal and visual modes
+vim.keymap.set({ "n", "v" }, "gl", "$", { desc = "Go to End of Line" })
+
+local cmp = require("cmp")
+
+vim.keymap.set("i", "<C-x>", function()
+  cmp.complete()
+end, { desc = "Trigger CMP Completion" })
---
+++ lua/plugins/surround.lua
@@ -0,0 +1,8 @@
+return {
+  "echasnovski/mini.surround",
+  version = false, -- use latest
+  event = "VeryLazy",
+  config = function()
+    require("mini.surround").setup()
+  end,
+}
