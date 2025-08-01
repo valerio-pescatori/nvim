34875d57e06c19a21e86c1038189a0d27f65ecdd
valerio-pescatori <pescatorim14@gmail.com>
2025-08-01 14:30:16 +0200

    switch to catppuccin, add prev/next hunk keybinds

--- lua/valerio/plugins/colorscheme.lua
+++ lua/valerio/plugins/colorscheme.lua
@@ -1,22 +1,8 @@
 return {
-  "folke/tokyonight.nvim",
-  priority = 1000,
-  config = function()
-    local transparent = true -- set to true if you would like to enable transparency
-
-    require("tokyonight").setup({
-      style = "night",
-      transparent = transparent,
-      styles = {
-        sidebars = transparent and "transparent" or "dark",
-        floats = transparent and "transparent" or "dark",
-      },
-      on_colors = function()
-      end,
-      on_highlights = function()
-      end
-    })
-
-    vim.cmd("colorscheme tokyonight")
-  end,
+	"catppuccin/nvim",
+	name = "catppuccin",
+	priority = 1000,
+	config = function()
+		vim.cmd("colorscheme catppuccin-mocha")
+	end,
 }
--- lua/valerio/plugins/git-signs.lua
+++ lua/valerio/plugins/git-signs.lua
@@ -1,40 +1,60 @@
 return {
-  {
-    "lewis6991/gitsigns.nvim",
-    config = function()
-      local git_signs = require("gitsigns")
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			local git_signs = require("gitsigns")
 
-      git_signs.setup({
-        signs = {
-          add          = { text = "│" },
-          change       = { text = "│" },
-          delete       = { text = "_" },
-          topdelete    = { text = "‾" },
-          changedelete = { text = "~" },
-        },
-        numhl = false,             -- Highlight numbers for changes
-        linehl = false,            -- Highlight lines for changes
-        current_line_blame = true, -- Show blame inline by default
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = 'eol', -- 'eol' | 'overlay' | 'right_align'
-          delay = 1000,
-          ignore_whitespace = false,
-          virt_text_priority = 100,
-          use_focus = true,
-        },
-        sign_priority = 6,       -- Priority of signs
-        update_debounce = 100,   -- Debounce time for updates (in ms)
-        max_file_length = 40000, -- Max file length to enable gitsigns
+			git_signs.setup({
+				signs = {
+					add = { text = "│" },
+					change = { text = "│" },
+					delete = { text = "_" },
+					topdelete = { text = "‾" },
+					changedelete = { text = "~" },
+				},
+				numhl = false, -- Highlight numbers for changes
+				linehl = false, -- Highlight lines for changes
+				current_line_blame = true, -- Show blame inline by default
+				current_line_blame_opts = {
+					virt_text = true,
+					virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+					delay = 1000,
+					ignore_whitespace = false,
+					virt_text_priority = 100,
+					use_focus = true,
+				},
+				sign_priority = 6, -- Priority of signs
+				update_debounce = 100, -- Debounce time for updates (in ms)
+				max_file_length = 40000, -- Max file length to enable gitsigns
 
-        on_attach = function(bufnr)
-          -- Add keymaps
-          vim.keymap.set("n", "<leader>gp", "<cmd>:Gitsigns preview_hunk<CR>",
-            { buffer = bufnr, desc = "Preview changes" })
-          vim.keymap.set("n", "<leader>gb", "<cmd>:Gitsigns toggle_current_line_blame<CR>",
-            { buffer = bufnr, desc = "Blame" })
-        end
-      })
-    end
-  }
+				on_attach = function(bufnr)
+					local function map(mode, l, r, desc)
+						vim.keymap.set(mode, l, r, { buffer = bufnr, desc = desc })
+					end
+					local gs = package.loaded.gitsigns
+
+					-- Add keymaps
+					map("n", "<leader>gp", "<cmd>:Gitsigns preview_hunk<CR>", "Preview changes")
+
+					map("n", "<leader>gb", "<cmd>:Gitsigns toggle_current_line_blame<CR>", "Toggle line blame")
+
+					map("n", "]h", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "]c", bang = true })
+						else
+							gs.nav_hunk("next")
+						end
+					end, "Next Hunk")
+
+					map("n", "[h", function()
+						if vim.wo.diff then
+							vim.cmd.normal({ "[c", bang = true })
+						else
+							gs.nav_hunk("prev")
+						end
+					end, "Prev Hunk")
+				end,
+			})
+		end,
+	},
 }
